84a9b67164138d5dbcd804e7771083fd6e1e830a
alexBob <bboybrelock@gmail.com>
2021-03-29 11:51:42 +0300

    task 2 done

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,4 +4,5 @@ import program from 'commander';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.6')
-  
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
